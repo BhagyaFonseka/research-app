a41252076a376a5dff51dfdb69067bfc545aa22f
NIRMANI-SPECTRE\Bhagya <bhagyaf000@gmail.com>
2024-03-17 10:50:36 +0530

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <div class="container my-5 mx-auto">
   <div class="p-5 text-center bg-body-tertiary rounded-3">
     <h1 class="text-body-emphasis">Basic jumbotron</h1>
+    <button type="button">search</button>
     <p class="lead">
       This is a simple Bootstrap jumbotron that sits within a <code>.container</code>, recreated with built-in utility classes.
     </p>
